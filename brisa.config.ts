59378e20c9d81d7d4d7fd74f3f7232f2a24046e3
Aral Roca <aral-rg@hotmail.com>
2024-09-08 16:20:00 +0200

    chore: update versions

--- brisa.config.ts
+++ brisa.config.ts
@@ -2,7 +2,7 @@ import type { Configuration } from "brisa";
 
 export default {
   // If you want to use Brisa's as static site generator, you can set
-  // the output to "static" (default: "server").
-  // Also "desktop", "android" and "ios" are available.
-  output: "server",
+  // the output to "static" (default: "bun").
+  // Also "node", "desktop", "android" and "ios" are available.
+  output: "bun",
 } satisfies Configuration;
